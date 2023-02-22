871a5f3000eee22bf37c5676e0dab34fe1be0401
abhishekchandar <30390489+abhishekchandar@users.noreply.github.com>
2023-02-22 18:16:56 -0500

    Update urls.py

--- monitor/urls.py
+++ monitor/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 from rest_framework import routers
 
+#api/classify is the API endpoint for ResNet inference. 
 urlpatterns = [
     path('api/classify', UploadView.as_view(), name = 'prediction')
     # path('api/upload/ct', CTUploadView.as_view(), name = 'ct_prediction'),
