4b49aa28d4115b1826fafa54476c752053cfcdfc
abhishekchandar <30390489+abhishekchandar@users.noreply.github.com>
2023-02-22 18:16:17 -0500

    Update apps.py

--- monitor/apps.py
+++ monitor/apps.py
@@ -9,6 +9,7 @@ class MonitorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'monitor'
 
+#Class defining the object representing ResNet model
 class ResNetModelConfig(AppConfig):
     name = 'resnetAPI'
     MODEL_FILE = os.path.join(settings.MODELS, "entire_model.pt")
