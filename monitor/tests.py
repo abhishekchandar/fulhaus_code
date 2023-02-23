07e9e2717ee027d76ee3d04f5d887b9edd1eb974
Abhishek Chandar <abhishekchandar23@gmail.com>
2023-02-22 20:42:16 -0500

    Unit testing for CI/CD

--- monitor/tests.py
+++ monitor/tests.py
@@ -4,11 +4,19 @@ import requests
 
 url = "http://127.0.0.1:8000/api/classify"
 script_dir = os.path.dirname(os.path.abspath(__file__))
-class CreateTestForAPI(TestCase):
+class CreateValidTestForAPI(TestCase):
     def test_upload_request(self):
             form = {
                 "image": open(os.path.join(script_dir,"Abner PU Swivel Chair.jpg"),'rb')
             }
             response = requests.post(url,files=form)
             self.assertEqual(response.status_code, 201)
+
+class CreateInValidTestForAPI(TestCase):
+    def test_upload_request(self):
+            form = {
+                "image": "monitor\Abner PU Swivel Chair.jpg"
+            }
+            response = requests.post(url,files=form)
+            self.assertEqual(response.status_code, 500)
 
